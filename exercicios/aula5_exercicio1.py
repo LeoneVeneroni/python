43f48d37adbe972fda7d3c705c4593f4713cc984
LeoneVeneroni <48731326+LeoneVeneroni@users.noreply.github.com>
2019-06-07 20:19:32 -0300

    Delete aula5_exercicio1.py

--- exercicios/aula5_exercicio1.py
+++
@@ -1,8 +0,0 @@
-def maximo():
-    a1 = float(input('Insira um número inteiro: '))
-    a2 = float(input('Insira outro número inteiro: '))
-    a3 = float(input('Insira mais um número inteiro: '))
-    m = max(a1, a2, a3)
-    print('Maior número =', m)
-    
-maximo()    
